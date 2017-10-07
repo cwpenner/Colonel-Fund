00b29ca939ed9b5a665f04b173e4920f6dfab36a
Chris Penner <chriswpenner@me.com>
2017-10-06 21:02:14 -0600

    Rough app template

---
+++ app/src/main/java/com/colonelfund/colonelfund/EventListActivity.java
@@ -0,0 +1,13 @@
+package com.colonelfund.colonelfund;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+    }
+}
