7df28f169289206206e8ba6ce73d4087cd9b288c
jwilken1 <jwilken1@asu.edu>
2017-12-03 20:16:55 -0500

    Added FB back to main

--- Android App/ColonelFund/app/src/main/java/com/colonelfund/colonelfund/MainActivity.java
+++ Android App/ColonelFund/app/src/main/java/com/colonelfund/colonelfund/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
     /**
