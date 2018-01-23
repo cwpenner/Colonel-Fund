d16d3cda1d3d68ff586bc2b8ce1241fd70a56712
jwilken1 <jwilken1@asu.edu>
2018-01-23 02:12:30 -0500

    About You Screen Changes

--- Android App/ColonelFund/app/src/main/java/com/colonelfund/colonelfund/MainActivity.java
+++ Android App/ColonelFund/app/src/main/java/com/colonelfund/colonelfund/MainActivity.java
@@ -3,6 +3,8 @@ package com.colonelfund.colonelfund;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -38,8 +40,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.about_you) {
+            Intent intent = new Intent(this, ViewProfileActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.your_history_events) {
+            Intent intent = new Intent(this, ViewProfileActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      *
      * @param view
