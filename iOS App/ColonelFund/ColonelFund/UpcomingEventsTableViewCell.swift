785c9704030f3d9e7e9fc0d912f751f04bf5ba37
Chris Penner <cwpenner@asu.edu>
2018-03-28 18:38:23 -0600

    Tab controller functional. My Events and Associated Events in View Member screen now have updated and prettier event lists

---
+++ iOS App/ColonelFund/ColonelFund/UpcomingEventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UpcomingEventsTableViewCell.swift
+//  ColonelFund
+//
+//  Created by Chris Penner on 3/28/18.
+//  Copyright © 2018 PennerTech. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingEventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
